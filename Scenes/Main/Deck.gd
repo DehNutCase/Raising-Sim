ef06ebbaca8eeec88afda01e6798213550c8f011
DehNutCase <dslayerz@hotmail.com>
2025-05-31 16:24:08 -0700

    Performance optimization.

--- Scenes/Main/Deck.gd
+++ Scenes/Main/Deck.gd
@@ -1,14 +1,20 @@
 extends GridContainer
 
 var locations = Constants.locations
-var buttons = []
+var cache = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visibility_changed.connect(update_buttons)
 
 func update_buttons():
+	if cache.hash() == Player.card_game_deck.hash():
+		return
+	for node in get_children():
+		remove_child(node)
+		node.queue_free()
 	for card: CardResource in Player.card_game_deck:
+		cache.append(card)
 		var button: CardButton = load("res://Scenes/UI/Actions/card_button.tscn").instantiate()
 		add_child(button)
 		button.card_ui.card = card
