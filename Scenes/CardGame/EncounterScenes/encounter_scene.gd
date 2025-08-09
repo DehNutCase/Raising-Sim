7906ccb5a206669a26ad00bd3598a5c48deaeee6
DehNutCase <dslayerz@hotmail.com>
2025-08-08 17:39:57 -0700

    Card game GUI changes.

--- Scenes/CardGame/EncounterScenes/encounter_scene.gd
+++ Scenes/CardGame/EncounterScenes/encounter_scene.gd
@@ -3,4 +3,6 @@ extends Control
 
 @export var background: Texture2D
 @export_multiline var victory_reward: String #Use JSON formatted string, will be parsed
+@export_multiline var starting_status_enemy: String #Use JSON formatted string, will be parsed
+@export_multiline var starting_status_player: String #Use JSON formatted string, will be parsed
 @export var background_music: AudioStream
