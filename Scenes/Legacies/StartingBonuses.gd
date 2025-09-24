0200b0b9bf3eb76df25412ab79a3380a4469438e
DehNutCase <dslayerz@hotmail.com>
2025-09-24 11:59:10 -0700

    Legacy display bugfix.

--- Scenes/Legacies/StartingBonuses.gd
+++ Scenes/Legacies/StartingBonuses.gd
@@ -7,8 +7,10 @@ func _ready() -> void:
 	visibility_changed.connect(display_stats)
 	
 func display_stats():
-	text = "[table=2][cell padding=32,0,32,0]New Game Bonus Stats:[/cell][cell padding=32,0,32,0][/cell][cell padding=32,0,32,0][/cell][cell padding=32,0,32,0][/cell][cell padding=32,0,32,0][/cell][cell padding=32,0,32,0][/cell]"
+	label.clear()
+	text = "[table=2][cell padding=32,0,32,0]Starting Bonus Stats:[/cell][cell padding=32,0,32,0][/cell][cell padding=32,0,32,0][/cell][cell padding=32,0,32,0][/cell][cell padding=32,0,32,0][/cell][cell padding=32,0,32,0][/cell]"
 	var font_size = Config.get_config("CustomSettings", "FontSize")
+	
 	var stats = Player.new_game_plus_bonuses.get("stats", {})
 	for stat in stats:
 		var label = stat
@@ -18,11 +20,10 @@ func display_stats():
 			label += " (" + Constants.stats[stat].emoji + ")"
 		if ("icon" in Constants.stats[stat]):
 			label += " ([img=" + str(font_size) + "]" + Constants.stats[stat].icon + "[/img])"
-			if stat == "experience":
-				text += "[cell padding=32,0,32,0]" + label + ": " + str(stats[stat])
-			else:
-				text += "[cell padding=32,0,32,0]" + label + ": " + str(stats[stat]) + "[/cell]"
+		if stat == "experience":
+			text += "[cell padding=32,0,32,0]" + label + ": " + str(stats[stat])
+		else:
+			text += "[cell padding=32,0,32,0]" + label + ": " + str(stats[stat]) + "[/cell]"
 	
 	text += "[/table]\n"
-	label.clear()
 	label.append_text(text)
